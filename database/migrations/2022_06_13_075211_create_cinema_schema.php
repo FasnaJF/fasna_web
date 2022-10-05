77126a59f85ab75809ff21851be32fe798396d00
jffasna <fasna@rhino-partners.com>
2022-10-05 22:08:04 +0530

    migration for cinemas added

--- database/migrations/2022_06_13_075211_create_cinema_schema.php
+++ database/migrations/2022_06_13_075211_create_cinema_schema.php
@@ -6,37 +6,52 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateCinemaSchema extends Migration
 {
-    /** ToDo: Create a migration that creates all tables for the following user stories
-
-    For an example on how a UI for an api using this might look like, please try to book a show at https://in.bookmyshow.com/.
-    To not introduce additional complexity, please consider only one cinema.
-
-    Please list the tables that you would create including keys, foreign keys and attributes that are required by the user stories.
-
-    ## User Stories
-
-     **Movie exploration**
-     * As a user I want to see which films can be watched and at what times
-     * As a user I want to only see the shows which are not booked out
-
-     **Show administration**
-     * As a cinema owner I want to run different films at different times
-     * As a cinema owner I want to run multiple films at the same time in different showrooms
-
-     **Pricing**
-     * As a cinema owner I want to get paid differently per show
-     * As a cinema owner I want to give different seat types a percentage premium, for example 50 % more for vip seat
-
-     **Seating**
-     * As a user I want to book a seat
-     * As a user I want to book a vip seat/couple seat/super vip/whatever
-     * As a user I want to see which seats are still available
-     * As a user I want to know where I'm sitting on my ticket
-     * As a cinema owner I dont want to configure the seating for every show
-     */
     public function up()
     {
-        throw new \Exception('implement in coding task 4, you can ignore this exception if you are just running the initial migrations.');
+        /*Moives*/
+        Schema::create('movies', function($table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->text('synopsis')->nullable();
+            $table->timestamps();
+        });
+
+        /*Shows*/
+        Schema::create('shows', function($table) {
+            $table->increments('id');
+            $table->string('show_room');
+            $table->double('ticket_price', 8, 2);
+            $table->dateTime('show_time');
+            $table->double('duration');
+            $table->boolean('booking_status')->default(false);
+            $table->integer('movie_id')->unsigned();
+            $table->foreign('movie_id')->references('id')->on('movies');
+            $table->timestamps();
+        });
+
+        /*Seating*/
+        Schema::create('seating_types', function($table) {
+            $table->increments('id');
+            $table->enum('type', ['vip', 'couple', 'super_vip', 'usual']);
+            $table->integer('seat_number');
+            $table->timestamps();
+        });
+
+        Schema::create('bookings', function($table) {
+            $table->id('id');
+            $table->foreignId('show_id')->references('id')->on('shows');
+            $table->timestamps();
+        });
+
+        Schema::create('seat_allocations', function($table) {
+            $table->id('id');
+            $table->foreignId('movie_id')->references('id')->on('movies');
+            $table->foreignId('show_id')->references('id')->on('shows');
+            $table->foreignId('booking_id')->references('id')->on('bookings');
+            $table->double('discount');
+            $table->boolean('is_allocated', 1)->default(false);
+        });
+
     }
 
     /**
